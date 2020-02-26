6f58d358949680a94e5302ed678ff9b5d5d8293d
elioq <elio@cn.163>
2020-02-26 16:51:36 +0800

    添加项目到本地仓库

---
+++ src/test/GitTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+    }
+
+
+}
