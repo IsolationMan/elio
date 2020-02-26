3867eb9d62efcf33a5de1660c541c261a177c5ce
elioq <elio@cn.163>
2020-02-26 17:10:46 +0800

    修改后

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -4,9 +4,23 @@ public class GitTest {
 
     public static void main(String[] args) {
 
+        int a=6;
         System.out.println("11111111");
         System.out.println("11111111");
         System.out.println("11111111");
+
+        test(5);
+
+        System.out.println(a);
+
+    }
+
+    public static void test(int a){
+
+         a+=10;
+        System.out.println(a);
+        System.out.println("新增");
+
     }
 
 
